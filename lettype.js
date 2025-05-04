1474ede783d09d0125ddf8e3839245697488226a
sauravmunde <souravmunde2001@gmail.com>
2025-05-04 11:31:19 +0530

    first commit

---
+++ lettype.js
@@ -0,0 +1,40 @@
+
+/*  if the java script code is 
+ add let type , let is add the 
+ number value or add the string 
+ value in this code
+
+*/
+
+ // let variable
+ /* 
+let x =42;
+let y = x;
+x = 43;
+console.log(y);
+
+/*  const variable the value 
+is not change the programs  */
+/*
+const a =400;
+console.log(a);
+
+const pi = 3.14;
+console.log(pi);
+
+/*  var variable  */
+/*
+var c = 70;
+var d = "name"
+console.log(c);
+console.log(d);
+
+*/
+// output off the following code 
+let x = 9;
+const y = 10;
+ // x = y;
+// console.log(x);
+
+  y = x;
+console.log(y); 
