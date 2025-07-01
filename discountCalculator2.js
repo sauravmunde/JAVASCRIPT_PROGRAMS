9ab9dfd3de736a859064216fd9427ceee867cdf1
sauravmunde <souravmunde2001@gmail.com>
2025-07-01 15:26:37 +0530

    first commit

---
+++ discountCalculator2.js
@@ -0,0 +1,22 @@
+
+function discountCalculator(totalAmount) {
+
+    let discount = 0;
+
+    if(totalAmount >= 100) {
+
+        discount = 10
+
+    }else if (totalAmount >= 50) {
+
+        discount = 5
+
+    }else{
+
+        return totalAmount;
+    }
+    discount = totalAmount * (discount/100);
+    return totalAmount - discount;
+
+}
+console.log(discountCalculator(80))
